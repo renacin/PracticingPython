0f368b73b6918259e962bec6ea91aa3f29e72753
renacin <renacin.matadeen@ryerson.ca>
2021-03-06 12:28:55 -0500

    Setup Complete

---
+++ PracticeQuestions/main.py
@@ -0,0 +1,19 @@
+# Name:                                            Renacin Matadeen
+# Date:                                               03/06/2021
+# Title                             Various Practice Questions: Improving Python Skills
+#
+# ----------------------------------------------------------------------------------------------------------------------
+from Questions.questions import *
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Main Function That Will Store Everything
+def main():
+
+    # Import Question Class & Run Number
+    basic_questions.question_1()
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Main Entry Point Into Python Code
+if __name__ == "__main__":
+    main()
